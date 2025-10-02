0a97f43e59a672a88e9d87352fc517c3c2091c5f
Tir <fathirfatiha07@gmail.com>
2025-10-02 16:23:47 +0700

    b

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -4,7 +4,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 import '../css/app.css'
 import '../css/sb-admin-2-custom.css'
 import '../css/crud.css'
-import '../css/card.css'   // 🔥 tambahin ini  // ❄️ lebih
+import '../css/card.css'   // 🔥 tambahin ini
 import React from 'react'
 import { createInertiaApp } from '@inertiajs/react'
 import { createRoot } from 'react-dom/client'
@@ -18,4 +18,16 @@ createInertiaApp({
   setup({ el, App, props }) {
     createRoot(el).render(<App {...props} />)
   },
+// pake spinner 👍
+  progress: {
+    delay: 250,
+    showSpinner: true,
+    color: '#4B9CE2',
+    includeCSS: true,
+  }
+})
+
+NProgress.configure({
+  speed: 1000,
+  trickleSpeed: 2000
 })
