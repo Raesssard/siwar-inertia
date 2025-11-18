cedcbc84938bda78719eb0587829d14cdd326089
Raesssard <nfelisx@gmail.com>
2025-11-18 08:27:57 +0700

    a

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,9 +4,7 @@ namespace App\Providers;
 
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Session;
 use Inertia\Inertia;
 
 class AppServiceProvider extends ServiceProvider
@@ -18,33 +16,10 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-Log::info("AppServiceProvider booted!");
-
-        if (Auth::check()) {
-            $user = Auth::user();
-
-            // Jika belum ada active_role → ambil role pertama
-            if (!Session::has('active_role')) {
-                $role = $user->roles->first()->name ?? null;
-                Session::put('active_role', $role);
-            }
-        }
         Carbon::setLocale('id');
 
         // Share global data ke semua halaman Inertia
         Inertia::share([
-            // 🔹 Data user login
-            'auth' => function () {
-                /** @var User $user */
-                $user = Auth::user();
-                return $user ? [
-                    'id' => $user->id,
-                    'name' => $user->name,
-                    'roles' => $user->getRoleNames()->toArray(),
-                ] : null;
-            },
-
-            // 🔹 Flash message (success, error, info)
             'flash' => function () {
                 return [
                     'success' => session('success'),
